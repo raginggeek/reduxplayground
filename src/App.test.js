9621f6dc1f7db267fa34743bdfa9475891f10fcb
Geoff Finch <gfinch1@gmail.com>
2018-02-26 08:06:07 -0600

    fixed original unit test.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import {createStore} from 'redux';
+import reducer from './reducers/index';
+import { Provider } from 'react-redux';
 
 it('renders without crashing', () => {
+  let store = createStore(reducer);
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+    ReactDOM.render(<Provider store={store}><App /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
